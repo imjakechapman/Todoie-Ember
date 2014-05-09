ebd06d519cf92a2d2145274b04d0f804f540c099
Jake Chapman <jake@imjakechapman>
2014-05-09 11:40:29 -0700

    Added Procfile for heroku app, and updated db url for new mongolab testing db.

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 // config/database.js
 module.exports = {
-
-  'url': 'mongodb://nodejitsu:3957469f5be4b5cdcd9fcb2adadead75@troup.mongohq.com:10095/nodejitsudb7238832605'
-
+  'url': 'mongodb://testinguser:testinguser@oceanic.mongohq.com:10095/app25046861'
 }
